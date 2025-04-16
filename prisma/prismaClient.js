a42cfb1aa98a76d2ff981e3a06d0016071f28181
littlesluggerboi <tayaotaoaeron@gmail.com>
2025-04-17 00:59:32 +0800

    Changed client and adjusted schema.

--- prisma/prismaClient.js
+++ prisma/prismaClient.js
@@ -1,6 +1,14 @@
-import { PrismaClient } from "@prisma/client";
+import { neonConfig } from '@neondatabase/serverless'
+import { PrismaClient } from '@prisma/client'
+import { PrismaNeon } from '@prisma/adapter-neon'
+import ws from 'ws'
+import dotenv from 'dotenv'
 
+dotenv.config()
+neonConfig.webSocketConstructor = ws;
+const connectionString = `${process.env.DATABASE_URL}`
 
-const prisma = new PrismaClient();
+const adapter = new PrismaNeon({ connectionString })
+const prisma = new PrismaClient({ adapter })
 
 export default prisma;
