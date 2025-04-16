38edd438e3489949721e11b259229f76133e2928
littlesluggerboi <tayaotaoaeron@gmail.com>
2025-04-17 01:01:55 +0800

    Modified indexRoutes.

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -3,38 +3,31 @@ import signupValidator from "../validator/signupValidator.js";
 import prisma from "../prisma/prismaClient.js";
 import bodyValidator from "../validator/bodyValidator.js";
 import bcrypt from "bcrypt";
-import passport from "../passport/passportConfig.js";
+import passport from "passport";
+import authController from "../controller/authController.js";
 
 const indexRouter = Router();
 
-indexRouter.get("/log-in", async (req, res, next) => {
+indexRouter.get("/login", async (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return res.redirect("/clubs/myclubs");
+  }
   res.render("loginPage.ejs", {
     title: "Clubs | Login Page",
   });
 });
 
 indexRouter.post(
-  "/log-in", passport.authenticate("local"), async (req, res, next) => {
-    res.redirect("/")
-  }
+  "/login",
+  passport.authenticate("local", { successRedirect: "/clubs/myclubs" })
 );
 
-indexRouter.get("/sign-up", async (req, res, next) => {
+indexRouter.get("/signup", async (req, res, next) => {
   res.render("signupPage.ejs", { title: "Clubs | Sign Up Page" });
 });
 
-
-indexRouter.post("/log-out", async (req, res, next) => {
-  req.logOut((err) => {
-    if (err) {
-      return next(err);
-    }
-    res.redirect("/log-in");
-  });
-});
-
 indexRouter.post(
-  "/sign-up",
+  "/signup",
   signupValidator,
   bodyValidator,
   async (req, res, next) => {
@@ -48,7 +41,7 @@ indexRouter.post(
       await prisma.member.create({
         data,
       });
-      res.redirect("/log-in");
+      res.redirect("/login");
     } catch (error) {
       next(error);
     }
@@ -56,7 +49,20 @@ indexRouter.post(
 );
 
 indexRouter.get("/", async (req, res, next) => {
-  res.redirect("/log-in");
+  res.render("homePage.ejs", { title: "Clubs | Join the Clubs" });
 });
+
+indexRouter.post(
+  "/logout",
+  authController.authenticateMiddleware,
+  async (req, res, next) => {
+    req.logOut((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/login");
+    });
+  }
+);
 
 export default indexRouter;
