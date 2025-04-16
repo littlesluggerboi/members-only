459af418e24aa7305dfe741cd8df5b696cd8c14a
littlesluggerboi <tayaotaoaeron@gmail.com>
2025-04-17 00:59:05 +0800

    Adjusted passport configurations.

--- passport/passportConfig.js
+++ passport/passportConfig.js
@@ -13,14 +13,14 @@ passport.use(
           where: { email: givenUsername },
         });
         if (!queriedUser) {
-          return done(new Error("No account"), false);
+          return done(null, false, "Email is not associated with a user.");
         }
         const { password, ...user } = queriedUser;
         const match = await bcrypt.compare(givenPassword, password);
         if (match) {
           return done(null, user);
         }
-        return done(new Error("Wrong Password", false));
+        return done(null, false, "Wrong Password given.");
       } catch (error) {
         return done(error);
       }
@@ -29,12 +29,10 @@ passport.use(
 );
 
 passport.serializeUser((user, done) => {
-  console.log("Serialize: ", user);
   return done(null, user.id);
 });
 
 passport.deserializeUser(async (id, done) => {
-  console.log("Deserialize: ", id);
   try {
     const user = await prisma.member.findUnique({
       where: { id },
